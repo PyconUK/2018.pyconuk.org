6d5b93bc933f886a851d5c7e372e291856761a67
Alex Chan <alex@alexwlchan.net>
2018-01-03 07:54:47 +0000

    Don't force push when deploying

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@
 set -o errexit
 set -o nounset
 
-git checkout gh-pages
 ./build.sh
+git checkout gh-pages
 git add .
 git commit -m "[skip ci] Auto-commit. Build latest changes."
 git push origin gh-pages
