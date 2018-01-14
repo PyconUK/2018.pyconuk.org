4a118e57d9f74e4b6d7e0946e39c3ae1ba11b69f
Daniele Procida <daniele.procida@divio.ch>
2018-01-14 14:22:10 +0000

    install djangocms-transfer 0.0.8

--- settings.py
+++ settings.py
@@ -16,6 +16,7 @@ INSTALLED_ADDONS = [
     'djangocms-snippet',
     'djangocms-style',
     'djangocms-text-ckeditor',
+    'djangocms-transfer',
     'djangocms-video',
     'django-filer',
     # </INSTALLED_ADDONS>
