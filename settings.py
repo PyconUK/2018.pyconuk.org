a70bbd694772247623b8d164c93b33e5e506a412
Daniele <daniele.procida@divio.ch>
2018-01-16 17:16:47 +0000

    Updated BS-related components

--- settings.py
+++ settings.py
@@ -7,7 +7,6 @@ INSTALLED_ADDONS = [
     'aldryn-sso',
     'aldryn-django-cms',
     'aldryn-devsync',
-    'djangocms-bootstrap4',
     'djangocms-file',
     'djangocms-googlemap',
     'djangocms-history',
@@ -32,4 +31,20 @@ aldryn_addons.settings.load(locals())
 
 INSTALLED_APPS.extend([
     'djangocms_icon',
+    'djangocms_bootstrap4',
+    "djangocms_bootstrap4.contrib.bootstrap4_alerts",
+    "djangocms_bootstrap4.contrib.bootstrap4_badge",
+    "djangocms_bootstrap4.contrib.bootstrap4_card",
+    "djangocms_bootstrap4.contrib.bootstrap4_carousel",
+    "djangocms_bootstrap4.contrib.bootstrap4_collapse",
+    "djangocms_bootstrap4.contrib.bootstrap4_grid",
+    "djangocms_bootstrap4.contrib.bootstrap4_jumbotron",
+    "djangocms_bootstrap4.contrib.bootstrap4_link",
+    "djangocms_bootstrap4.contrib.bootstrap4_listgroup",
+    "djangocms_bootstrap4.contrib.bootstrap4_media",
+    "djangocms_bootstrap4.contrib.bootstrap4_picture",
+    "djangocms_bootstrap4.contrib.bootstrap4_tabs",
+    "djangocms_bootstrap4.contrib.bootstrap4_utilities",
+    "djangocms_bootstrap4.contrib.bootstrap4_content"
+
 ])
